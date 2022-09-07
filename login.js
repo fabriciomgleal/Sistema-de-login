f6d859a8476364a8df7f249f4d7eeb9157cba27e
fabricio <87091756+fabriciomgleal@users.noreply.github.com>
2022-09-07 14:58:49 -0300

    Atualização

--- login.js
+++ login.js
@@ -1,5 +1,29 @@
 function login(){
-    var email = document.getElementById("email");
+    let emailCad = document.querySelector("#emailCad")
+    let senhaCad = document.querySelector("#senhaCad")
+    let listaUser = []
+    let uservalid = {
+        email: '', 
+        senha: ''
+    }
+    listaUser = JSON.parse(localStorage.getItem('listaUser'))
+    listaUser.forEach((item) => {
+        if(email.value == item.emailCad && senha.value == item.senhaCad){
+            uservalid = {
+                email: item.emailCad,
+                senha: item.senhaCad
+            }
+        }
+    });
+    if(email.value == uservalid.email && senha.value == uservalid.senha){
+        let chave = Math.random().toString(16).substr(2) + Math.random().toString(16).substr(2)
+        localStorage.setItem("chave", chave);
+        window.location.href="index.html"
+    } else {
+        window.location.href="login.html"
+    }
+
+    /*var email = document.getElementById("email");
     var senha = document.getElementById("senha");
 
     if(email.value == "admin@admin.com" && senha.value == "admin"){
@@ -8,7 +32,7 @@ function login(){
     }
     else{
         alert("Usuario ou senha invalidos!");
-    }
+    }*/
 }
 
 function cadastrar(){
@@ -24,4 +48,14 @@ function logar(){
     )
     localStorage.setItem('listaUser', JSON.stringify(listaUser))
     window.location.href="login.html"
+}
+
+if(localStorage.getItem("chave") == null){
+    window.location.href="login.html"
+}
+
+function sair(){
+    localStorage.removeItem('chave')
+    window.location.href="login.html"
+
 }
--- main.js
+++
@@ -1,9 +0,0 @@
-var logado = false;
-if(localStorage.getItem("acesso") == "true"){
-    logado = true;
-}
-
-if(logado == false){
-    alert("Favor inserir o login e senha!");
-    window.location.href="login.html";
-}
